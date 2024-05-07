04b29bb4ad7ca68efc39ee49412e36d7c809a7e7
Twincasper <justinwatson651@gmail.com>
2024-05-07 14:23:29 -0400

    Edited init.js

--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -1,15 +1,19 @@
 const User = require('../models/User');
+const Organization = require('../models/Organization');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async (knex) => {
-  // Before you have models you can always just do `await knex('table_name').del`
   await knex('users').del();
 
   await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
 
-  await User.create('cool_cat', '1234');
-  await User.create('l33t-guy', '1234');
-  await User.create('wowow', '1234');
+  const org1Id = await Organization.query().where('name', 'Organization 1').select('id').first();
+  const org2Id = await Organization.query().where('name', 'Organization 2').select('id').first();
+
+  await User.create('cool_cat', '1234', org1Id);
+  await User.create('l33t-guy', '1234', org2Id);
+  await User.create('wowow', '1234', org1Id);
 };
