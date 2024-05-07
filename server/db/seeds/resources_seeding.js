a362c16c126a343f449116b0924148229b6dcbe2
Marcy <marcy@Marcys-MacBook-Pro.local>
2024-05-07 14:20:36 -0400

    seeds for resources

--- server/db/seeds/resources_seeding.js
+++ server/db/seeds/resources_seeding.js
@@ -2,8 +2,26 @@ exports.seed = async function(knex) {
     // Deletes ALL existing entries
     await knex('resources').del()
     await knex('resources').insert([
-      {colName: 'value', otherColName: 'value'},
-      {colName: 'value', otherColName: 'value'},
-      {colName: 'value', otherColName: 'value'}
+        {category: 'food', name: 'FoodUniteBank', description: 'Provides shelter for homeless individuals. Located at 123 Main Street, Anytown, USA. Contact John Doe at johndoe@example.com or +1 (555) 123-4567.'
+        },
+        {category: 'Clothing',
+          name: 'Threads of Hope',
+          description: 'Accepts and distributes gently used clothing, shoes, and accessories. Located at 789 Elm Street, Anystate, USA. Contact Sarah Johnson at sarahjohnson@example.com or +1 (555) 321-7890.'
+        },
+        {
+          category: 'Medical Services',
+          name: 'HealthCare Clinic',
+          description: 'Provides general checkups, vaccinations, and minor treatments. Located at 101 Maple Avenue, Anyvillage, USA. Contact Dr. Michael Lee at mlee@example.com or +1 (555) 456-7890.'
+        },
+        {
+          category: 'Support Groups',
+          name: 'Mental Health Support Circle',
+          description: 'Offers support for mental health issues. Virtual meetings held every Tuesday and Thursday at 7pm. Contact Maria Rodriguez at maria.rodriguez@example.com or +1 (555) 789-0123.'
+        },
+        {
+          category: 'Donation',
+          name: 'GiveBack Foundation',
+          description: 'Raises funds for providing education to underprivileged children.'
+        },
     ]);
   };
