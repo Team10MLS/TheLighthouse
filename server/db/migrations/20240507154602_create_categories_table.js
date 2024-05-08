df3b498bc08e06e388819151c2708505e2e3e04d
Marcy <marcy@Marcys-MacBook-Pro.local>
2024-05-08 11:33:20 -0400

    commiting

--- server/db/migrations/20240507154602_create_categories_table.js
+++ server/db/migrations/20240507154602_create_categories_table.js
@@ -14,5 +14,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-    knex.schema.dropTable('categories');
+    return knex.schema.dropTable('categories');
 };
