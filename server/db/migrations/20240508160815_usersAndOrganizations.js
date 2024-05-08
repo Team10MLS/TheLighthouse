80be415be0dba623972f9982aa29eb41895f4660
Twincasper <justinwatson651@gmail.com>
2024-05-08 12:24:20 -0400

    Established migrations and seeds for users, organizations

---
+++ server/db/migrations/20240508160815_usersAndOrganizations.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('organizations', (table) => {
+      table.increments();
+      table.string('name').notNullable().unique();
+  })
+  .createTable('users', (table) => {
+      table.increments();
+      table.string('username').notNullable().unique();
+      table.string('password_hash').notNullable();
+      table.integer('organization_id').unsigned().notNullable();
+      table.foreign('organization_id').references('id').inTable('organizations');
+  })
+};
+
+/**
+* @param { import("knex").Knex } knex
+* @returns { Promise<void> }
+*/
+exports.down = function (knex) {
+return knex.schema.dropTable('users').dropTable('organizations');
+};
---
+++ server/db/seeds/01_usersAndOrganizations.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('organizations').del()
+  await knex('users').del()
+
+  await knex.raw('ALTER SEQUENCE organizations_id_seq RESTART WITH 1');
+  await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+
+
+  await knex('organizations').insert([
+    {name: 'Soup Kitchen'},
+    {name: 'Coat Drive'},
+    {name: 'Care for the Homeless'},
+    {name: 'Grace Church in New York'},
+    {name: 'East River Shelter'},
+  ])
+
+  await knex('users').insert([
+    {username: 'cool_cat', password_hash: '1234', organization_id: 1},
+    {username: 'l33t-guy', password_hash: '1234', organization_id: 2},
+    {username: 'wowow', password_hash: '1234', organization_id: 3},
+  ]);
+};
