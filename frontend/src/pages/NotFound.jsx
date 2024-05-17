0f60d3033cb1e83e9ca058f8c3c3d3b8c30afff8
Twincasper <justinwatson651@gmail.com>
2024-05-17 15:44:41 -0400

    Refactored and restyled login page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,36 @@
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function NotFoundPage() {
-  return <>
-    <h1>Not Found</h1>
-  </>;
+  const navigate = useNavigate();
+  
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+      <main className="grid min-h-full h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-red-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link to="#" onClick={(e) => {
+            e.preventDefault();
+            navigate('/');
+          }}
+          className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:ring-2 focus-visible:ring-red-600"
+          >
+            Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -4,14 +4,10 @@ import { useState, useEffect } from "react";
 import ContributeModal from "../components/ContributeModal";
 import { useNavigate } from "react-router-dom"; // if we need to navigate to category page
 import PostForm from "../components/PostForm";
-
 const categories = ['Shelters', 'Food', 'Clothing', 'Medical Services', 'Support Groups', 'Donations & Fundraisings'];
-
-
 export default function ResourcesPage() {
   const [data, setData] = useState({ posts: [], resources: [] });
   const [searchTerm, setSearchTerm] = useState('');
-
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -21,44 +17,39 @@ export default function ResourcesPage() {
     resources: data.resources.filter(resource => resource.name.includes(searchTerm) || resource.description.includes(searchTerm))
   };
 
-
   const handleCategoryClick = async (category) => {
     const resources = await getAllResourcesByCategory(category);
-    setData({ ...data, resources });
+    setData({ posts:[], resources });
   };
+
 
   useEffect(() => {
     const fetchData = async () => {
       const { post: posts, resource: resources } = await getAllPostsAndResources();
       setData({ posts, resources });
     };
-
     fetchData();
-  }, []);
+  }, [data]);
 
   const handlePostSubmit = async (postData) => {
     // Create the post
     const newPost = await createPost(postData);
-
     // Update the state to include the new post
     setData(prevData => ({
       ...prevData,
       posts: [...prevData.posts, newPost]
     }));
   };
-
   // ... other code
-
   return (
     <>
-    <input type="text" placeholder="search" value={searchTerm} onChange={handleSearchChange}/>
+      <input type="text" placeholder="search" value={searchTerm} onChange={handleSearchChange} />
       {categories.map(category => (
         <button className="black-button" key={category} onClick={() => handleCategoryClick(category)}>{category}</button>
       ))}
       <ContributeModal />
       {/* Render the PostForm component */}
       <PostForm onSubmit={handlePostSubmit} />
-
       {filteredData.posts.map(post => (
         <div key={post.id}>
           <h2 className="text-2xl font-bold text-gray-900">{post.title}</h2>
