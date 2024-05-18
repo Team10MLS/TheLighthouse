c8df25566c5fbaa5f96d90c524eeb5478bbde77b
Twincasper <justinwatson651@gmail.com>
2024-05-18 16:25:43 -0400

    Added footer component we may use later and created comment-adapter.js

---
+++ frontend/src/adapters/comment-adapter.js
@@ -0,0 +1,22 @@
+import { fetchHandler,getPatchOptions, getPostOptions } from "../utils";
+
+const baseUrl = '/api';
+
+module.exports = commentRouter;
+
+export const createComment = async ({user_id, body, post_id }) => (
+  fetchHandler(`${baseUrl}/comments`, getPostOptions({user_id, body, post_id }))
+);
+
+export const deleteComment = async ({user_id, post_id }) => (
+  fetchHandler(`${baseUrl}/comments/${user_id}/${post_id}`, getPatchOptions({user_id, post_id }))
+);
+
+export const listCommentsForPost = async (post_id) => {
+  const [comments] = await fetchHandler(`${baseUrl}/comments/${post_id}`);
+  return comments || [];
+}
+
+export const editComment = async ({user_id, post_id, body }) => (
+  fetchHandler(`${baseUrl}/comments`, getPatchOptions({user_id, post_id, body }))
+);
