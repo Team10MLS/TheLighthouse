0acdff4d4f4db6f78731be019d5a2c90c2584843
Twincasper <justinwatson651@gmail.com>
2024-05-19 17:50:34 -0400

    Rebuilt output.css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="bg-">
+    <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
