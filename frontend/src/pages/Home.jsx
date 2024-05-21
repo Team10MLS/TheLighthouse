0600a8e0ff5aa832dcb8fd9d22f9870e19ff2de8
Twincasper <justinwatson651@gmail.com>
2024-05-21 11:22:32 -0400

    Added debounce to searching and separated posts and resources states for performance

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -222,7 +222,7 @@ export default function Home() {
                     </div>
                     <div className="relative">
                       <img
-                        src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
+                        src="https://pyxis.nymag.com/v1/imgs/d18/d6a/e5ab478ed12f148b4039938805745e2bf5-church-of-the-ascension.rvertical.w570.jpg"
                         alt=""
                         className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       />
@@ -232,7 +232,7 @@ export default function Home() {
                   <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                     <div className="relative">
                       <img
-                        src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+                        src="https://static01.nyt.com/images/2014/11/18/nyregion/COATFLOATER/COATFLOATER-tmagArticle.jpg"
                         alt=""
                         className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       />
@@ -240,7 +240,7 @@ export default function Home() {
                     </div>
                     <div className="relative">
                       <img
-                        src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                        src="https://www.foodforsoul.it/wp-content/uploads/2021/02/HotBreadChefCollective-MaryamInKitchen2-scaled-e1614066123507-1920x931.jpg"
                         alt=""
                         className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       />
