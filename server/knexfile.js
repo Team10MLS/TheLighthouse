4cf76e27e9a10ac96c97d5c48edacb71e0708a21
Twincasper <justinwatson651@gmail.com>
2024-05-25 18:49:55 -0400

    Updated knexfile.js to use pg_connection_string

--- server/knexfile.js
+++ server/knexfile.js
@@ -14,12 +14,12 @@ When we deploy in "production", we'll provide a PG_CONNECTION_STRING
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
+    connection: process.env.PG_CONNECTION_STRING || {
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
       password: process.env.PG_PASS || 'postgres',
-      database: process.env.PG_DB || 'project',
+      database: process.env.PG_DB || 'postgres',
     },
     migrations: {
       directory: migrationsDirectory,
